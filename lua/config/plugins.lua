38eb915a9ee306086015935fb9e66d1a82cfe0e8
Warbacon <joaquinguerratocino@gmail.com>
2023-11-09 14:57:51 +0100

    Fix jsonls

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -252,7 +252,11 @@ return {
                 },
             })
 
+            --Enable (broadcasting) snippet capability for completion
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            capabilities.textDocument.completion.completionItem.snippetSupport = true
             require('lspconfig').jsonls.setup {
+                capabilities = capabilities,
                 settings = {
                     json = {
                         schemas = require('schemastore').json.schemas(),

340cd8cba7f1c2f83fbda087f4f07c990ed3f2b0
Warbacon <joaquinguerratocino@gmail.com>
2024-01-03 22:59:49 +0100

    Disable builtin plugins

--- init.lua
+++ init.lua
@@ -10,15 +10,22 @@ require("keymaps")
 -- Lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup(require("plugins"))
+local plugins = require("plugins")
+local opts = {
+	performance = {
+		disabled_plugins = { "netrwPlugin", "tutor" },
+	},
+}
+
+require("lazy").setup(plugins, opts)

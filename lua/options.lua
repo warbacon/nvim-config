07c9235d648ef1a5cf6ea98807cdd23a3a3ac5ab
Warbacon <joaquinguerratocino@gmail.com>
2024-01-11 14:45:25 +0100

    change: remove "How to disable mouse" menu entry

--- lua/options.lua
+++ lua/options.lua
@@ -42,3 +42,9 @@ vim.opt.colorcolumn = "80"
 
 -- Disable banner in netrw
 vim.g.netrw_banner = 0
+
+-- Remove the "How-to disable mouse" menu item and the separator above it
+vim.cmd([[
+  aunmenu PopUp.How-to\ disable\ mouse
+  aunmenu PopUp.-1-
+]])

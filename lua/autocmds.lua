5ff003401c574ff3707cc65b9eeb9cbaef7908b6
Warbacon <joaquinguerratocino@gmail.com>
2024-01-19 20:18:01 +0100

    fix: cursor not being restored at exit

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -7,3 +7,8 @@ vim.api.nvim_create_autocmd("FileType", {
     set shiftwidth=2
     ]],
 })
+
+vim.api.nvim_create_autocmd("VimLeave", {
+    pattern = "*",
+    command=[[set guicursor= | call chansend(v:stderr, "\x1b[ q")]]
+})

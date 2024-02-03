83e13e025002ae94b4d78b38b1f16e7f14feba10
Warbacon <45147327+warbacon@users.noreply.github.com>
2024-02-03 23:12:16 +0100

    Keymap changes

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,7 +1,5 @@
 vim.keymap.set("v", "<leader>y", '"+y')
 vim.keymap.set("n", "<leader>yy", '<S-v>"+y')
 
-vim.keymap.set("n", "q:", "<nop>", { noremap = true, silent = true })
-vim.keymap.set("n", "Q", "<nop>", { noremap = true, silent = true })
-
-vim.keymap.set("n", "-", "<cmd>Explore<cr>")
+vim.keymap.set("n", "q:", "<nop>", { noremap = true })
+vim.keymap.set("n", "Q", "<nop>", { noremap = true })

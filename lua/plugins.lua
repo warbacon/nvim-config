1ecda474258a6a58d9abaa43e26c80c31a1e4c9a
Warbacon <45147327+warbacon@users.noreply.github.com>
2024-02-19 23:16:32 +0100

    Reformat fzf-lua spec

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -129,37 +129,13 @@ return {
 		"ibhagwan/fzf-lua",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		branch = "windows",
+        -- stylua: ignore
 		keys = {
-			{
-				"<Leader>ff",
-				function()
-					require("fzf-lua").files()
-				end,
-				mode = "n",
-			},
-			{
-				"<Leader>fg",
-				function()
-					require("fzf-lua").live_grep()
-				end,
-				mode = "n",
-			},
-			{
-				"<Leader>fh",
-				function()
-					require("fzf-lua").help_tags()
-				end,
-				mode = "n",
-			},
-			{
-				"<Leader>fb",
-				function()
-					require("fzf-lua").buffers()
-				end,
-				mode = "n",
-			},
+			{ "<Leader>ff", function() require("fzf-lua").files() end, mode = "n" },
+			{ "<Leader>fg", function() require("fzf-lua").live_grep() end, mode = "n" },
+			{ "<Leader>fh", function() require("fzf-lua").help_tags() end, mode = "n" },
+			{ "<Leader>fb", function() require("fzf-lua").buffers() end, mode = "n" },
 		},
-
 		config = function()
 			require("fzf-lua").setup({ "telescope" })
 		end,

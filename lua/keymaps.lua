2a7d000cd98981d951a4fa9c37346c2ef53ea8c6
Warbacon <45147327+warbacon@users.noreply.github.com>
2024-02-27 09:28:08 +0100

    Add esc to disable hlsearch

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -3,3 +3,5 @@ vim.keymap.set("n", "<leader>yy", '<S-v>"+y')
 
 vim.keymap.set("n", "q:", "<nop>", { noremap = true })
 vim.keymap.set("n", "Q", "<nop>", { noremap = true })
+
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")

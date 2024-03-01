9e2063cf91e680762c9842249fdafa0c9782086c
Warbacon <45147327+warbacon@users.noreply.github.com>
2024-03-01 23:43:40 +0100

    Change lazy.nvim bootstrap style

--- init.lua
+++ init.lua
@@ -14,17 +14,19 @@ require("autocmds")
 -- Lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Plugins
-require("lazy").setup("plugins", {
-	change_detection = {
-		notify = false,
-	},
-})
+require("lazy").setup("plugins", { change_detection = { notify = false, }, })
 
 -- Add hyprlang filetype
 vim.filetype.add({

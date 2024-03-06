bbfce10eba3f55b2466e623779b2c9f8b121b24e
Warbacon <45147327+warbacon@users.noreply.github.com>
2024-03-06 19:33:50 +0100

    Remove trailing new lines

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,9 +1,18 @@
--- Remove traling whitespace on save
+-- Remove traling whitespace and new lines on save
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 	callback = function()
-		local save_cursor = vim.fn.getpos(".")
-		vim.cmd([[%s/\s\+$//e]])
-		vim.fn.setpos(".", save_cursor)
+		-- Save cursor position to later restore
+		local curpos = vim.api.nvim_win_get_cursor(0)
+		-- Search and replace trailing whitespace
+		vim.cmd([[keeppatterns %s/\s\+$//e]])
+		vim.api.nvim_win_set_cursor(0, curpos)
+
+		-- Remove new lines
+		local n_lines = vim.api.nvim_buf_line_count(0)
+		local last_nonblank = vim.fn.prevnonblank(n_lines)
+		if last_nonblank + 1 < n_lines then
+			vim.api.nvim_buf_set_lines(0, last_nonblank + 1, n_lines, true, {})
+		end
 	end,
 })
 

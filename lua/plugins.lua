3370a48816d32aac26bb100747478f21204c6df1
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-03-18 17:45:40 +0100

    Add Neogit

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,4 +1,17 @@
 return {
+	-- Neogit
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional - Diff integration
+
+			-- Only one of these is needed, not both.
+			"nvim-telescope/telescope.nvim", -- optional
+		},
+		opts = {},
+	},
+
 	-- Catppuccin
 	{
 		"catppuccin/nvim",

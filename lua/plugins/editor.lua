9ee93ec373c9b3e038951f9fa164fe11429c11e4
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-03-21 00:32:40 +0100

    feat: readd dressing.nvim (using lazy load)

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -43,6 +43,24 @@ return {
         ft = "kitty",
     },
 
+    -- DRESSING.NVIM ----------------------------------------------------------
+    {
+        "stevearc/dressing.nvim",
+        lazy = true,
+        init = function()
+            ---@diagnostic disable-next-line: duplicate-set-field
+            vim.ui.select = function(...)
+                require("lazy").load({ plugins = { "dressing.nvim" } })
+                return vim.ui.select(...)
+            end
+            ---@diagnostic disable-next-line: duplicate-set-field
+            vim.ui.input = function(...)
+                require("lazy").load({ plugins = { "dressing.nvim" } })
+                return vim.ui.input(...)
+            end
+        end,
+    },
+
     -- TELESCOPE.NVIM ---------------------------------------------------------
     {
         "nvim-telescope/telescope.nvim",

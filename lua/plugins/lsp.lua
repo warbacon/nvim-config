09de0949928c210cd5a80f71f8abd86093918238
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-03-21 16:50:19 +0100

    fix: gqq does not work

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -61,7 +61,6 @@ return {
         dependencies = {
             "mason.nvim",
             { "folke/neodev.nvim", opts = {} },
-            -- { "j-hui/fidget.nvim", opts = { progress = { display = { render_limit = 0 } } } },
         },
         config = function()
             require("mason-lspconfig").setup()
@@ -73,6 +72,7 @@ return {
             vim.api.nvim_create_autocmd("LspAttach", {
                 group = vim.api.nvim_create_augroup("UserLspConfig", {}),
                 callback = function(ev)
+                    vim.bo[ev.buf].formatexpr = nil  -- makes gq work
                     local opts = { buffer = ev.buf }
                     vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
                     vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)

754f0ee2bc824584ed90a96b3e721c60b3f7cab7
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-03-21 01:12:14 +0100

    Add gitcommit treesitter parser

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -11,6 +11,7 @@ return {
                 "cpp",
                 "diff",
                 "fish",
+                "gitcommit",
                 "hyprlang",
                 "json",
                 "jsonc",

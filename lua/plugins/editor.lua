d97303e18f8a3eabdcabe5c9dba75f9469f0c2a7
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-03-23 20:42:24 +0100

    feat: add ts-context-commentstring

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -140,11 +140,22 @@ return {
     -- COMMENT.NVIM -----------------------------------------------------------
     {
         "numToStr/Comment.nvim",
+        dependencies = {
+            {
+                "JoosepAlviste/nvim-ts-context-commentstring",
+                opts = { enable_autocmd = false },
+            },
+        },
         keys = {
             { "gc", mode = { "n", "v" } },
             { "gb", mode = { "n", "v" } },
         },
-        opts = { mappings = { extra = false } },
+        config = function()
+            require("Comment").setup({
+                mappings = { extra = false },
+                pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+            })
+        end,
     },
 
     -- NVIM-COLORIZER.LUA -----------------------------------------------------

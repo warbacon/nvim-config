5bf0836a008774941f6350334330de074b569ec7
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-04-12 18:09:26 +0200

    Switch back to gitsigns

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,29 +1,4 @@
 return {
-    -- MINI.DIFF --------------------------------------------------------------
-    {
-        "echasnovski/mini.diff",
-        event = "LazyFile",
-        keys = {
-            {
-                "gho",
-                function()
-                    require("mini.diff").toggle_overlay(0)
-                end,
-                mode = "n",
-            },
-        },
-        opts = {
-            view = {
-                style = "sign",
-                signs = {
-                    add = "▎",
-                    change = "▎",
-                    delete = "",
-                },
-            },
-        },
-    },
-
     -- MINI.SURROUND ----------------------------------------------------------
     {
         "echasnovski/mini.surround",
@@ -36,6 +11,66 @@ return {
         "echasnovski/mini.ai",
         event = "VeryLazy",
         opts = {},
+    },
+
+    -- GITSIGNS ---------------------------------------------------------------
+    {
+        "lewis6991/gitsigns.nvim",
+        event = "LazyFile",
+        opts = {
+            on_attach = function(bufnr)
+                local gitsigns = require("gitsigns")
+
+                local function map(mode, l, r, opts)
+                    opts = opts or {}
+                    opts.buffer = bufnr
+                    vim.keymap.set(mode, l, r, opts)
+                end
+
+                -- Navigation
+                map("n", "]c", function()
+                    if vim.wo.diff then
+                        vim.cmd.normal({ "]c", bang = true })
+                    else
+                        gitsigns.nav_hunk("next")
+                    end
+                end)
+
+                map("n", "[c", function()
+                    if vim.wo.diff then
+                        vim.cmd.normal({ "[c", bang = true })
+                    else
+                        gitsigns.nav_hunk("prev")
+                    end
+                end)
+
+                -- Actions
+                map("n", "<leader>hs", gitsigns.stage_hunk)
+                map("n", "<leader>hr", gitsigns.reset_hunk)
+                map("v", "<leader>hs", function()
+                    gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+                end)
+                map("v", "<leader>hr", function()
+                    gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+                end)
+                map("n", "<leader>hS", gitsigns.stage_buffer)
+                map("n", "<leader>hu", gitsigns.undo_stage_hunk)
+                map("n", "<leader>hR", gitsigns.reset_buffer)
+                map("n", "<leader>hp", gitsigns.preview_hunk_inline)
+                map("n", "<leader>hb", function()
+                    gitsigns.blame_line({ full = true })
+                end)
+                map("n", "<leader>tb", gitsigns.toggle_current_line_blame)
+                map("n", "<leader>hd", gitsigns.diffthis)
+                map("n", "<leader>hD", function()
+                    gitsigns.diffthis("~")
+                end)
+                map("n", "<leader>td", gitsigns.toggle_deleted)
+
+                -- Text object
+                map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+            end,
+        },
     },
 
     -- COMMENT.NVIM -----------------------------------------------------------

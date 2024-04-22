4e6056cce468d4fe5bfd1131e499b66119b3c49c
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-04-22 14:14:56 +0200

    docs: document new options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,33 +1,48 @@
+-- Enable line numbers and relative line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+-- Enable persistent undo with undo files
 vim.opt.undofile = true
 
+-- Set split windows to open below and to the right
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 
+-- Set a column indicator at 80 characters
+vim.opt.colorcolumn = "80"
+
+-- Set popup menu height
+vim.opt.pumheight = 10
+
+-- Use spaces for tabs and configure indentation settings
 vim.opt.expandtab = true
 vim.opt.shiftround = true
 vim.opt.shiftwidth = 4
 vim.opt.smartindent = true
 vim.opt.tabstop = 4
 
+-- Set scrolloff and sidescroll settings
 vim.opt.scrolloff = 999
-vim.opt.sidescroll = 5
+vim.opt.sidescroll = 4
 vim.opt.wrap = false
 
-vim.opt.list = true
-vim.opt.listchars = { tab = "  ", trail = "·", nbsp = "␣" }
-
+-- Enable block mode for virtual editing
 vim.opt.virtualedit = "block"
 
+-- Customize messages and display options
 vim.opt.shortmess:append({ I = true })
-
 vim.opt.signcolumn = "yes"
 
+-- Ignore case in searches unless an uppercase letter is included
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
+-- Enable visible whitespace and customize characters
+vim.opt.list = true
+vim.opt.listchars = { tab = "  ", trail = "·", nbsp = "␣" }
+
+-- Customize display of various elements
 vim.opt.fillchars = {
     foldopen = "",
     foldclose = "",
@@ -36,7 +51,3 @@ vim.opt.fillchars = {
     diff = "╱",
     eob = " ",
 }
-
-vim.opt.colorcolumn = "80"
-
-vim.opt.pumheight = 10

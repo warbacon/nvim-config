549de30426978c49d5eceff68253030c7c71e257
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-04-28 19:09:46 +0200

    feat: better autocmds

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,11 +1,31 @@
+local autocmd = vim.api.nvim_create_autocmd
+
 -- Fix cursor at leave
-vim.api.nvim_create_autocmd("VimLeave", {
-    command = [[set guicursor= | call chansend(v:stderr, "\x1b[ q")]],
+autocmd("VimLeave", {
+    callback = function()
+        vim.opt.guicursor = ""
+        vim.api.nvim_chan_send(vim.api.nvim_get_vvar("stderr"), "\x1b[ q")
+    end,
 })
 
 -- Highlight when yanking text
-vim.api.nvim_create_autocmd("TextYankPost", {
+autocmd("TextYankPost", {
     callback = function()
         vim.highlight.on_yank()
+    end,
+})
+
+-- Makes gqq work in Markdown
+autocmd("LspAttach", {
+    pattern = { "*.md" },
+    callback = function()
+        vim.bo.formatexpr = nil
+    end,
+})
+
+-- Disable auto comment after hitting o in normal mode
+autocmd("FileType", {
+    callback = function()
+        vim.opt.formatoptions:remove({ "o" })
     end,
 })

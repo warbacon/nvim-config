9576d8258d81fd2d7d4aeb70f4abbb1071f5fa15
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-05-02 21:00:35 +0200

    misc: fix deprecation in conform

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -12,8 +12,8 @@ return {
     },
     opts = {
         formatters_by_ft = {
-            c = { "clang_format" },
-            cpp = { "clang_format" },
+            c = { "clang-format" },
+            cpp = { "clang-format" },
             fish = { "fish_indent" },
             lua = { "stylua" },
             python = { "ruff_format" },

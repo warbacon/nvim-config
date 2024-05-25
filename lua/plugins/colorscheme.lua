06dd41a9a6dece329e45a4fab4ccd3f7c4d4bd5c
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-05-25 13:44:40 +0200

    Remove fancy cursor

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,12 +6,6 @@ return {
         priority = 1000,
         opts = {
             styles = { booleans = { "bold" } },
-            custom_highlights = function(colors)
-                return {
-                    Cursor2 = { fg = colors.green, bg = colors.green },
-                    IndentLineCurrent = { fg = colors.sky },
-                }
-            end,
             integrations = {
                 fidget = true,
                 mason = true,
@@ -28,7 +22,6 @@ return {
         config = function(_, opts)
             require("catppuccin").setup(opts)
             vim.cmd.colorscheme("catppuccin")
-            vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:block-Cursor2/lCursor2,r-cr-o:hor20"
         end,
     },
 }

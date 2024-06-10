73bac22df540bbbf8bea3b2e5731148a8df6d133
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-06-10 16:55:20 +0200

    feat: add nvim-colorizer

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -31,6 +31,21 @@ return {
         end,
     },
 
+    -- NVIM-COLORIZER.LUA ======================================================
+    {
+        "NvChad/nvim-colorizer.lua",
+        event = "LazyFile",
+        opts = { user_default_options = { names = false } },
+        config = function(_, opts)
+            require("colorizer").setup(opts)
+
+            -- execute colorizer as soon as possible
+            vim.defer_fn(function()
+                require("colorizer").attach_to_buffer(0)
+            end, 0)
+        end,
+    },
+
     -- GITSIGNS.NVIM ===========================================================
     {
         "lewis6991/gitsigns.nvim",

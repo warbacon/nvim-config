22b921afbcf2521cddfff45f366d30c36b675095
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-07-16 17:50:26 +0200

    fix: run conform.nvim synchronously

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -6,7 +6,7 @@ return {
         {
             "<leader>bf",
             function()
-                require("conform").format({ async = true, lsp_fallback = true })
+                require("conform").format({ lsp_fallback = true })
             end,
         },
     },

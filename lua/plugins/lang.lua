c4784412f5c4e684dde1a76c7d93849679606545
Warbacon <45147327+warbacon@users.noreply.github.com>
2024-08-05 22:23:56 +0200

    perf: only load markdown-preview in markdown files

--- lua/plugins/lang.lua
+++ lua/plugins/lang.lua
@@ -3,7 +3,7 @@ return {
     {
         "iamcco/markdown-preview.nvim",
         cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-        ft = "markdown",
+        ft = { "markdown" },
         build = function()
             vim.fn["mkdp#util#install"]()
         end,

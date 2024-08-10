25b76da51b58ba3694714287d33f20a73a764a3e
Warbacon <45147327+warbacon@users.noreply.github.com>
2024-08-10 19:27:56 +0200

    Only load LazyFile event if lazy.nvim is enabled

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -21,28 +21,3 @@ autocmd("FileType", {
         vim.opt.formatoptions:remove({ "o", "r" })
     end,
 })
-
--- This autocmd will only trigger when a file was loaded from the cmdline.
--- It will render the file as quickly as possible.
-autocmd("BufReadPost", {
-    once = true,
-    callback = function(event)
-        -- Skip if we already entered vim
-        if vim.v.vim_did_enter == 1 then
-            return
-        end
-
-        -- Try to guess the filetype (may change later on during Neovim startup)
-        local ft = vim.filetype.match({ buf = event.buf })
-        if ft then
-            -- Add treesitter highlights and fallback to syntax
-            local lang = vim.treesitter.language.get_lang(ft)
-            if not (lang and pcall(vim.treesitter.start, event.buf, lang)) then
-                vim.bo[event.buf].syntax = ft
-            end
-
-            -- Trigger early redraw
-            vim.cmd([[redraw]])
-        end
-    end,
-})

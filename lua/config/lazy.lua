f180dddf0ff25ff85474a43a333e3032005a2bbd
Warbacon <45147327+warbacon@users.noreply.github.com>
2024-08-20 20:28:07 +0200

    refactor: improve lazy.nvim init

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,13 +1,17 @@
+-- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable",
-        lazypath,
-    })
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -15,7 +19,11 @@ vim.opt.rtp:prepend(lazypath)
 local Event = require("lazy.core.handler.event")
 Event.mappings.File = { id = "File", event = { "BufReadPost", "BufNewFile" } }
 
-require("lazy").setup("plugins", {
+-- Setup lazy.nvim
+require("lazy").setup({
+    spec = {
+        { import = "plugins" },
+    },
     install = {
         colorscheme = { "tokyonight" },
     },
@@ -33,7 +41,6 @@ require("lazy").setup("plugins", {
                 -- "matchparen",
                 -- "netrwPlugin",
                 "rplugin",
-                "spellfile",
                 "tarPlugin",
                 "tohtml",
                 "tutor",

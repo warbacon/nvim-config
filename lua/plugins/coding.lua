371c673809bec0c2eac116ca565cee5c3b6935f9
Warbacon <45147327+warbacon@users.noreply.github.com>
2024-08-29 13:27:20 +0200

    feat: better cmp mappings

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -164,9 +164,8 @@ return {
                     end,
                 },
                 mapping = cmp.mapping.preset.insert({
-                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<C-k>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-j>"] = cmp.mapping.scroll_docs(4),
                     ["<CR>"] = cmp.mapping.confirm({ select = true }),
                     ["<S-CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace }),
                     ["<C-CR>"] = function(fallback)

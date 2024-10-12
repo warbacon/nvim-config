963d346a8adae63ac3325ee5dab65e31ed3199e3
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-10-12 23:57:12 +0200

    feat: add nvim-ts-autotag

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,44 +1,57 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPost", "BufNewFile", "VeryLazy" },
-    lazy = vim.fn.argc(-1) == 0,
-    build = ":TSUpdate",
-    main = "nvim-treesitter.configs",
-    opts = {
-        highlight = { enable = true },
-        indent = { enable = true },
-        ensure_installed = {
-            "bash",
-            "c",
-            "cpp",
-            "css",
-            "fish",
-            "git_config",
-            "html",
-            "hyprlang",
-            "ini",
-            "java",
-            "javascript",
-            "json",
-            "jsonc",
-            "lua",
-            "markdown",
-            "markdown_inline",
-            "powershell",
-            "python",
-            "query",
-            "rasi",
-            "toml",
-            "vim",
-            "vimdoc",
-            "xml",
-            "yaml",
-            "zathurarc",
+    -- TRESITTER ===============================================================
+    {
+        "nvim-treesitter/nvim-treesitter",
+        event = { "BufReadPost", "BufNewFile", "VeryLazy" },
+        lazy = vim.fn.argc(-1) == 0,
+        build = ":TSUpdate",
+        main = "nvim-treesitter.configs",
+        opts = {
+            highlight = { enable = true },
+            indent = { enable = true },
+            ensure_installed = {
+                "bash",
+                "c",
+                "cpp",
+                "css",
+                "fish",
+                "git_config",
+                "html",
+                "hyprlang",
+                "ini",
+                "java",
+                "javascript",
+                "json",
+                "jsonc",
+                "lua",
+                "markdown",
+                "markdown_inline",
+                "powershell",
+                "python",
+                "query",
+                "rasi",
+                "toml",
+                "vim",
+                "vimdoc",
+                "xml",
+                "yaml",
+                "zathurarc",
+            },
         },
+        config = function(_, opts)
+            vim.filetype.add({ pattern = { [".*/dunst/dunstrc"] = "dosini" } })
+            require("nvim-treesitter.install").prefer_git = false
+            require("nvim-treesitter.configs").setup(opts)
+        end,
     },
-    config = function(_, opts)
-        vim.filetype.add({ pattern = { [".*/dunst/dunstrc"] = "dosini" } })
-        require("nvim-treesitter.install").prefer_git = false
-        require("nvim-treesitter.configs").setup(opts)
-    end,
+
+    -- TS-AUTOTAG ==============================================================
+    {
+        "windwp/nvim-ts-autotag",
+        config = function()
+            vim.defer_fn(function()
+                require("nvim-ts-autotag").setup()
+            end, 1)
+        end,
+    },
 }

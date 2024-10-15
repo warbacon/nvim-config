16708cffdf1240b120c350fbaca0e35b3f36e34b
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-10-15 20:40:38 +0200

    feat: better (hopefully) jdtls setup

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -48,15 +48,19 @@ return {
             "mason.nvim",
         },
         config = function()
-            local config = {
-                cmd = { vim.fn.exepath("jdtls") },
-                root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
-            }
-
             vim.api.nvim_create_autocmd("FileType", {
                 group = vim.api.nvim_create_augroup("start_jdtls", { clear = true }),
                 pattern = { "java" },
                 callback = function()
+                    local fname = vim.api.nvim_buf_get_name(0)
+                    local default_config = require("lspconfig.configs.jdtls").default_config
+                    default_config.cmd[1] = vim.fn.exepath("jdtls")
+
+                    local config = {
+                        cmd = default_config.cmd,
+                        root_dir = default_config.root_dir(fname)
+                    }
+
                     require("jdtls").start_or_attach(config)
                 end,
             })

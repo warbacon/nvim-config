314dcb92d8efe44136173df2e1543a3dbf6b2fb8
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-10-21 08:32:12 +0200

    feat: add icons to diagnostics

--- lua/config/extra.lua
+++ lua/config/extra.lua
@@ -13,3 +13,15 @@ vim.filetype.add({
         },
     },
 })
+
+-- Diagnostics configuration
+vim.diagnostic.config({
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = "",
+            [vim.diagnostic.severity.WARN] = "",
+            [vim.diagnostic.severity.HINT] = "",
+            [vim.diagnostic.severity.INFO] = "󰋽",
+        },
+    },
+})

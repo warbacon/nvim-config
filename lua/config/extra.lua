409e16f5d353b08d20ee4333233608e8b5268de0
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-10-21 10:44:04 +0200

    feat!: just disable treesitter on large files

--- lua/config/extra.lua
+++ lua/config/extra.lua
@@ -1,27 +1,11 @@
--- Bigfile support
-vim.filetype.add({
-    pattern = {
-        [".*"] = {
-            function(path, buf)
-                return vim.bo[buf]
-                        and vim.bo[buf].filetype ~= "bigfile"
-                        and path
-                        and vim.fn.getfsize(path) > 1024 * 1024 * 1.5 -- 1.5 MB
-                        and "bigfile"
-                    or nil
-            end,
-        },
-    },
-})
-
 -- Diagnostics configuration
 vim.diagnostic.config({
     signs = {
         text = {
-            [vim.diagnostic.severity.ERROR] = "",
-            [vim.diagnostic.severity.WARN] = "",
-            [vim.diagnostic.severity.HINT] = "",
-            [vim.diagnostic.severity.INFO] = "󰋽",
+            [vim.diagnostic.severity.ERROR] = require("util.icons").diagnostics.ERROR,
+            [vim.diagnostic.severity.WARN] = require("util.icons").diagnostics.WARN,
+            [vim.diagnostic.severity.INFO] = require("util.icons").diagnostics.INFO,
+            [vim.diagnostic.severity.HINT] = require("util.icons").diagnostics.HINT,
         },
     },
 })

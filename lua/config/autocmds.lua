781406294af68909e25a7f016416b735c4da1b48
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-10-21 08:38:33 +0200

    feat: add a few autocmds

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -22,6 +22,28 @@ autocmd("VimLeave", {
     end,
 })
 
+-- Resize splits if window got resized
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+    group = augroup("resize_splits"),
+    callback = function()
+        local current_tab = vim.fn.tabpagenr()
+        vim.cmd("tabdo wincmd =")
+        vim.cmd("tabnext " .. current_tab)
+    end,
+})
+
+-- Auto create dir when saving a file, in case some intermediate directory does not exist
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+    group = augroup("auto_create_dir"),
+    callback = function(event)
+        if event.match:match("^%w%w+:[\\/][\\/]") then
+            return
+        end
+        local file = vim.uv.fs_realpath(event.match) or event.match
+        vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+    end,
+})
+
 -- This autocmd will only trigger when a file was loaded from the cmdline.
 -- It will render the file as quickly as possible.
 vim.api.nvim_create_autocmd("BufReadPost", {

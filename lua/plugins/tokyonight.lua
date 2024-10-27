54aab6c56ed6387e99aecc57317fafbc207e18e8
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-10-27 19:23:51 +0100

    feat: remove cursor styling

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,11 +1,7 @@
 return {
     "folke/tokyonight.nvim",
     priority = 1000,
-    opts = {
-        on_highlights = function(highlights, colors)
-            highlights.CursorInsert = { bg = colors.green }
-        end,
-    },
+    opts = {},
     config = function(_, opts)
         require("tokyonight").setup(opts)
         vim.cmd.colorscheme("tokyonight")

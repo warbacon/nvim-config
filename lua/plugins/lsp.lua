357a0d7c4d7948aa17ecc1f3c3996f5e8e35c11a
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-11-03 22:04:34 +0100

    feat: replace nvim-cmp capabilities with blink's

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -23,12 +23,10 @@ return {
             local function attach_jdtls()
                 local fname = vim.api.nvim_buf_get_name(0)
 
-                local has_cmp_lsp, cmp_lsp = pcall(require, "cmp_nvim_lsp")
-
                 local config = {
                     cmd = { vim.fn.exepath("jdtls") },
                     root_dir = require("lspconfig.configs.jdtls").default_config.root_dir(fname),
-                    capabilities = has_cmp_lsp and cmp_lsp.default_capabilities() or nil,
+                    capabilities = require("blink-cmp").get_lsp_capabilities()
                 }
 
                 require("jdtls").start_or_attach(config)
@@ -50,22 +48,13 @@ return {
             "mason.nvim",
         },
         config = function()
-            local has_cmp_lsp, cmp_lsp = pcall(require, "cmp_nvim_lsp")
-
-            local capabilities = vim.tbl_deep_extend(
-                "force",
-                {},
-                vim.lsp.protocol.make_client_capabilities(),
-                has_cmp_lsp and cmp_lsp.default_capabilities() or {}
-            )
-
             local servers = require("util.lsp").servers
 
             servers.jdtls = nil
 
-            for server_name in pairs(servers) do
-                local server_opts = vim.tbl_deep_extend("force", { capabilities = capabilities }, servers[server_name])
-                require("lspconfig")[server_name].setup(server_opts)
+            for server, opts in pairs(servers) do
+                opts.capabilities = require("blink-cmp").get_lsp_capabilities(opts.capabilities)
+                require("lspconfig")[server].setup(opts)
             end
         end,
     },

901145d8e89446572ff047d7792c765857b4102a
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-11-03 19:49:19 +0100

    feat!: replace indentscope with indent-blanklines

---
+++ lua/plugins/indent-blankline.lua
@@ -0,0 +1,21 @@
+return {
+    "lukas-reineke/indent-blankline.nvim",
+    event = { "BufPostRead", "BufNewFile" },
+    main = "ibl",
+    ---@module "ibl"
+    ---@type ibl.config
+    opts = {
+        indent = {
+            char = "▏",
+            tab_char = "▏",
+        },
+        scope = { show_start = false, show_end = false },
+        exclude = {
+            filetypes = {
+                "help",
+                "lazy",
+                "mason",
+            },
+        },
+    },
+}

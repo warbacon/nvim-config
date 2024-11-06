a480ca74004096da5995e2ae0d1c77bc0fb2c215
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-11-06 18:29:28 +0100

    feat: use better signs for gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -2,6 +2,21 @@ return {
     "lewis6991/gitsigns.nvim",
     event = "LazyFile",
     opts = {
+        signs = {
+            add = { text = "▎" },
+            change = { text = "▎" },
+            delete = { text = "" },
+            topdelete = { text = "" },
+            changedelete = { text = "▎" },
+            untracked = { text = "▎" },
+        },
+        signs_staged = {
+            add = { text = "▎" },
+            change = { text = "▎" },
+            delete = { text = "" },
+            topdelete = { text = "" },
+            changedelete = { text = "▎" },
+        },
         attach_to_untracked = true,
         on_attach = function(bufnr)
             local gitsigns = require("gitsigns")

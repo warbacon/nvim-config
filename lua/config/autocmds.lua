06a6f55a29dcd62f0b25e3c29a0675155047acc1
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-11-08 23:33:05 +0100

    style: use autocmd function

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -23,7 +23,7 @@ autocmd("VimLeave", {
 })
 
 -- Auto create dir when saving a file, in case some intermediate directory does not exist
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+autocmd({ "BufWritePre" }, {
     group = augroup("auto_create_dir"),
     callback = function(event)
         if event.match:match("^%w%w+:[\\/][\\/]") then

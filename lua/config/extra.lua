378447eb92ddbc9dd8fddb74f1a13c0412aa1438
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-11-10 20:44:47 +0100

    refactor: cleaner diagnostic icons setup

--- lua/config/extra.lua
+++ lua/config/extra.lua
@@ -1,11 +1,12 @@
 -- Diagnostics configuration
+local diagnostic_icons = require("util.icons").diagnostics
 vim.diagnostic.config({
     signs = {
         text = {
-            [vim.diagnostic.severity.ERROR] = require("util.icons").diagnostics.ERROR,
-            [vim.diagnostic.severity.WARN] = require("util.icons").diagnostics.WARN,
-            [vim.diagnostic.severity.INFO] = require("util.icons").diagnostics.INFO,
-            [vim.diagnostic.severity.HINT] = require("util.icons").diagnostics.HINT,
+            [vim.diagnostic.severity.ERROR] = diagnostic_icons.ERROR,
+            [vim.diagnostic.severity.WARN] = diagnostic_icons.WARN,
+            [vim.diagnostic.severity.INFO] = diagnostic_icons.INFO,
+            [vim.diagnostic.severity.HINT] = diagnostic_icons.HINT,
         },
     },
 })

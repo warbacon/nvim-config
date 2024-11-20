82ed7aa90bccce030f63471a4c293c2984bd095e
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-11-20 13:56:41 +0100

    feat: enable oil delete_to_trash in windows

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -5,7 +5,7 @@ return {
         { "-", "<cmd>Oil<cr>" },
     },
     opts = {
-        delete_to_trash = vim.fn.has("win32") == 0,
+        delete_to_trash = true,
         skip_confirm_for_simple_edits = true,
         win_options = {
             colorcolumn = "",

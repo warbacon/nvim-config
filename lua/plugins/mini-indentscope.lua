d626d98130efcd11712cc757527bb1a4b733cf29
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-11-20 19:55:14 +0100

    fix: disable indentscope in markdown

--- lua/plugins/mini-indentscope.lua
+++ lua/plugins/mini-indentscope.lua
@@ -2,7 +2,7 @@ return {
     "echasnovski/mini.indentscope",
     event = "LazyFile",
     init = function()
-        local filetype_exclude = { "help", "oil" }
+        local filetype_exclude = { "markdown", "help", "oil" }
         vim.api.nvim_create_autocmd({ "BufEnter", "FileType" }, {
             callback = function()
                 if vim.bo.buftype == "nofile" or vim.tbl_contains(filetype_exclude, vim.bo.filetype) then

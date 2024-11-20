7721b98279d2725be707d146bb13e166f7a79b83
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-11-20 08:54:24 +0100

    feat: enable statuscolumn snacks module and use releases

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,8 +1,10 @@
 return {
     "folke/snacks.nvim",
+    version = "*",
     priority = 1000,
     opts = {
         notifier = { enabled = true },
         quickfile = { enabled = true },
+        statuscolumn = { enabled = true },
     },
 }

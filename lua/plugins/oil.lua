c1a7d59e3c025b9fa1e3f0b5d1e0fcf468e57fdd
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-11-21 23:00:26 +0100

    feat: more oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -7,10 +7,7 @@ return {
     opts = {
         delete_to_trash = true,
         skip_confirm_for_simple_edits = true,
-        lsp_file_methods = {
-            autosave_changes = "unmodified",
-        },
-        watch_for_changes = true,
+        lsp_file_methods = { autosave_changes = "unmodified" },
         keymaps = {
             ["`"] = false,
             [","] = "actions.cd",

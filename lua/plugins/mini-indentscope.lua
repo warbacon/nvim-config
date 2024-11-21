5315f8272882c24c0c1e7e3f962e21c9ddaf7fec
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-11-21 22:44:52 +0100

    feat!: enable indentscope always
    Maybe I remove it again

--- lua/plugins/mini-indentscope.lua
+++ lua/plugins/mini-indentscope.lua
@@ -1,18 +1,5 @@
 return {
     "echasnovski/mini.indentscope",
     event = "LazyFile",
-    init = function()
-        local filetype_exclude = { "markdown", "help", "oil" }
-        vim.api.nvim_create_autocmd({ "BufEnter", "FileType" }, {
-            callback = function()
-                if vim.bo.buftype == "nofile" or vim.tbl_contains(filetype_exclude, vim.bo.filetype) then
-                    vim.b.miniindentscope_disable = true
-                end
-            end,
-        })
-    end,
-    opts = {
-        symbol = "▏",
-        options = { border = 'top' }
-    },
+    opts = { symbol = "▏" },
 }

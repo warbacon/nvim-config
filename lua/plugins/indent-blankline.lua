8783ea6d4163db9dbad3a9aa4a83ea093b0701f3
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-11-22 15:29:14 +0100

    feat!: back to indent-blankline :(

---
+++ lua/plugins/indent-blankline.lua
@@ -0,0 +1,14 @@
+return {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {
+        indent = {
+            char = "▏",
+            tab_char = "▏",
+        },
+        scope = {
+            show_start = false,
+            show_end = false
+        },
+    },
+}
--- lua/plugins/mini-indentscope.lua
+++
@@ -1,5 +0,0 @@
-return {
-    "echasnovski/mini.indentscope",
-    event = "LazyFile",
-    opts = { symbol = "▏" },
-}

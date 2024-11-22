4bb514f547b1b011e13203102ba7034a06d2522a
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-11-22 15:30:03 +0100

    feat: add notifier keybind

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,6 +2,15 @@ return {
     "folke/snacks.nvim",
     version = "*",
     priority = 1000,
+    lazy = false,
+    keys = {
+        {
+            "<leader>n",
+            function()
+                Snacks.notifier.show_history()
+            end,
+        },
+    },
     opts = {
         notifier = { enabled = true },
         quickfile = { enabled = true },

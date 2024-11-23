7fa39adaf644cfe9cb276902681107b825ac63b7
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-11-23 20:55:49 +0100

    style: remove trailing comma in oil.lua

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -7,7 +7,7 @@ return {
     opts = {
         delete_to_trash = true,
         skip_confirm_for_simple_edits = true,
-        lsp_file_methods = { autosave_changes = "unmodified" },
+        lsp_file_methods = { autosave_changes = "unmodifid" },
         keymaps = {
             ["`"] = false,
             [","] = "actions.cd",

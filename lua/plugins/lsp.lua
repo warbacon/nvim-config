d8d82907d60d68ed1b5fad2f4e70cfbaee1350d2
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-11-26 22:18:35 +0100

    feat: use builtin uv mappings in lazydev

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,12 +8,11 @@ return {
         ft = "lua",
         opts = {
             library = {
-                { path = "luvit-meta/library", words = { "vim%.uv" } },
+                { path = "${3rd}/luv/library", words = { "vim%.uv" } },
                 { path = "snacks.nvim", words = { "Snacks" } },
             },
         },
     },
-    { "Bilal2453/luvit-meta", lazy = true },
 
     -- JDTLS ==================================================================
     {

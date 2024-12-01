ad1d2fd110c8e0698d5b1e769d3e143abcad9c92
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-12-01 20:39:45 +0100

    chore: ignore formatting

--- lua/plugins/mini-diff.lua
+++ lua/plugins/mini-diff.lua
@@ -2,6 +2,7 @@ return {
     "echasnovski/mini.diff",
     event = "LazyFile",
     keys = {
+        -- stylua: ignore
         { "<leader>hp", function() require("mini.diff").toggle_overlay(0) end },
     },
     opts = {

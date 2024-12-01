fb18543c07ca129cf296b80467fac67c8f8d697f
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-12-01 20:37:52 +0100

    feat: add signature help keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -15,6 +15,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
         vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
         vim.keymap.set("n", "gD", vim.lsp.buf.type_definition, opts)
         vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+        vim.keymap.set("i", "<c-h>", vim.lsp.buf.signature_help, opts)
     end,
 })
 

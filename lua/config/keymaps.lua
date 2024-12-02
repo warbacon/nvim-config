75b9ca63173d129380f730672f53c7dd7ac4a76e
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-12-02 11:33:54 +0100

    feat!: remove inline hints keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,9 +7,6 @@ vim.api.nvim_create_autocmd("LspAttach", {
     group = vim.api.nvim_create_augroup("lsp_actions", { clear = true }),
     callback = function(ev)
         local opts = { buffer = ev.buf }
-        vim.keymap.set("n", "<leader>H", function()
-            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = 0 }))
-        end)
         vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
         vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, opts)
         vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)

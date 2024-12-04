eb7abf37f2d88df61a97692197ce69d3326d4b32
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-12-04 20:05:20 +0100

    feat: improve mini.diff overlay keymap

--- lua/plugins/mini-diff.lua
+++ lua/plugins/mini-diff.lua
@@ -2,8 +2,19 @@ return {
     "echasnovski/mini.diff",
     event = "LazyFile",
     keys = {
-        -- stylua: ignore
-        { "<leader>hp", function() require("mini.diff").toggle_overlay(0) end },
+        {
+            "<leader>go",
+            function()
+                local overlay_enabled = vim.b.minidiff_overlay_enabled or false
+                require("mini.diff").toggle_overlay(0)
+                vim.b.minidiff_overlay_enabled = not overlay_enabled
+                vim.notify(
+                    overlay_enabled and "Overlay disabled!" or "Overlay enabled!",
+                    vim.log.levels.INFO,
+                    { title = "mini.diff" }
+                )
+            end,
+        },
     },
     opts = {
         view = { style = "sign" },

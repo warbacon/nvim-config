0ee77d37207463e78bbc3673ca6ad9e3aaeef7b8
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-12-07 23:32:19 +0100

    feat!: use neovim nightly native lsp keybinds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,12 +7,14 @@ vim.api.nvim_create_autocmd("LspAttach", {
     group = vim.api.nvim_create_augroup("lsp_actions", { clear = true }),
     callback = function(ev)
         local opts = { buffer = ev.buf }
-        vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-        vim.keymap.set("n", "<leader>cr", vim.lsp.buf.rename, opts)
         vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
         vim.keymap.set("n", "gD", vim.lsp.buf.type_definition, opts)
-        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-        vim.keymap.set("i", "<c-h>", vim.lsp.buf.signature_help, opts)
+        if vim.fn.has("nvim-0.11") == 0 then
+            vim.keymap.set("n", "gra", vim.lsp.buf.code_action, opts)
+            vim.keymap.set("n", "grn", vim.lsp.buf.rename, opts)
+            vim.keymap.set("n", "grr", vim.lsp.buf.references, opts)
+            vim.keymap.set("i", "<C-S>", vim.lsp.buf.signature_help, opts)
+        end
     end,
 })
 

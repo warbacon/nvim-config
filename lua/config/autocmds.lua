98b53acc8e0a071273da15afe714a439eb5bb721
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-12-26 20:31:06 +0100

    feat!: remove auto folder creation

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -21,15 +21,3 @@ autocmd("VimLeave", {
         io.write("\x1b[ q")
     end,
 })
-
--- Auto create dir when saving a file, in case some intermediate directory does not exist
-autocmd("BufWritePre", {
-    group = augroup("auto_create_dir"),
-    callback = function(event)
-        if event.match:match("^%w%w+:[\\/][\\/]") then
-            return
-        end
-        local file = vim.uv.fs_realpath(event.match) or event.match
-        vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
-    end,
-})

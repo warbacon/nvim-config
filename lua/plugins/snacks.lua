c32ec66b463b413a8eb6e03eb1a3c34bc4ee9897
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-01-15 01:37:15 +0100

    Add picker snack config (disabled)

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -5,11 +5,37 @@ return {
     keys = {
         -- stylua: ignore start
         { "<leader>n", function() Snacks.notifier.show_history() end },
+        -- { "<leader>,", function() Snacks.picker.buffers() end },
+        -- { "<leader>f", function() Snacks.picker.files() end },
+        -- { "<leader>f", function() Snacks.picker.files() end },
+        -- { "<leader>sg", function() Snacks.picker.grep() end },
+        -- { "<leader>sh", function() Snacks.picker.help() end },
+        -- { "<leader>sd", function() Snacks.picker.diagnostics() end },
+        -- { "<leader>ss", function() Snacks.picker.lsp_symbols() end },
+        -- { "<leader>ss", function() Snacks.picker.man() end },
         -- stylua: ignore end
     },
     opts = {
-        notifier = { enabled = true },
-        bigfile = { enabled = true },
-        quickfile = { enabled = true },
+        notifier = {},
+        bigfile = {},
+        quickfile = {},
+        picker = {
+            win = {
+                input = {
+                    keys = {
+                        ["<Esc>"] = { "close", mode = { "n", "i" } },
+                    },
+                },
+            },
+            icons = {
+                diagnostics = {
+                    Error = util.icons.diagnostics.ERROR,
+                    Warn = util.icons.diagnostics.WARN,
+                    Hint = util.icons.diagnostics.HINT,
+                    Info = util.icons.diagnostics.INFO,
+                },
+                kinds = util.icons.kinds,
+            },
+        },
     },
 }

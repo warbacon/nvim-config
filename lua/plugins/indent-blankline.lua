6b2b4f5679c343511d228c626e4c5ba74c704f5c
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-01-23 21:20:46 +0100

    fix: exclude snacks_picker_preview from indent-blankline

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -3,12 +3,17 @@ return {
     main = "ibl",
     event = "LazyFile",
     opts = {
+        exclude = {
+            filetypes = {
+                "snacks_picker_preview",
+            },
+        },
         indent = {
             char = "▏",
         },
         scope = {
             show_start = false,
             show_end = false,
-        }
+        },
     },
 }

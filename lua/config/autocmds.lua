193412e44d053018cf4970d57feabf9028beb73c
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-01-29 17:57:26 +0100

    autocmds: new treesitter autocmd and better descriptions

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -5,19 +5,27 @@ end
 
 local autocmd = vim.api.nvim_create_autocmd
 
--- Automatically highlight text after yanking it
 autocmd("TextYankPost", {
     group = augroup("highlight_yank"),
+    desc = "Automatically highlight text after yanking it",
     callback = function()
         (vim.hl or vim.highlight).on_yank()
     end,
 })
 
--- Fix the cursor when exiting Vim
 autocmd("VimLeave", {
     group = augroup("fix_cursor"),
+    desc = "Fix the cursor when exiting Vim",
     callback = function()
         vim.opt.guicursor = ""
         io.write("\x1b[ q")
     end,
+})
+
+autocmd("FileType", {
+    group = augroup("TreeSitter"),
+    desc = "Starts treesitter",
+    callback = function()
+        pcall(vim.treesitter.start)
+    end
 })

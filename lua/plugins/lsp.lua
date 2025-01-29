1ce8c659b4533a4953026c8270885103f351a388
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-01-29 03:27:35 +0100

    perf(lsp): use autocmds to setup lspconfig

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -18,7 +18,7 @@ return {
     -- JDTLS ==================================================================
     {
         "mfussenegger/nvim-jdtls",
-        enabled = vim.fn.executable("java") == 1,
+        enabled = vim.fn.executable("java") == 1 and require("config.lsp-servers").jdtls,
         ft = "java",
         config = function()
             local function attach_jdtls()
@@ -34,7 +34,7 @@ return {
             end
 
             vim.api.nvim_create_autocmd("FileType", {
-                group = vim.api.nvim_create_augroup("start-jdtls", { clear = true }),
+                group = vim.api.nvim_create_augroup("jdtls-setup", { clear = true }),
                 pattern = { "java" },
                 callback = attach_jdtls,
             })
@@ -49,12 +49,22 @@ return {
             "mason.nvim",
         },
         config = function()
-            local servers = vim.deepcopy(require("config.lsp-servers"))
+            local servers = vim.deepcopy(require("config.lsp-servers"), true)
 
             for server, opts in pairs(servers) do
                 if not opts.manual_setup then
                     opts.capabilities = require("blink-cmp").get_lsp_capabilities(opts.capabilities)
-                    require("lspconfig")[server].setup(opts)
+                    vim.api.nvim_create_autocmd("FileType", {
+                        once = true,
+                        group = vim.api.nvim_create_augroup(server .. "-setup", { clear = true }),
+                        pattern = opts.filetypes or require("lspconfig.configs." .. server).default_config.filetypes,
+                        callback = function(ev)
+                            require("lspconfig")[server].setup(opts)
+                            vim.defer_fn(function()
+                                vim.api.nvim_exec_autocmds("FileType", { buffer = ev.buf, group = "lspconfig" })
+                            end, 0)
+                        end,
+                    })
                 end
             end
         end,

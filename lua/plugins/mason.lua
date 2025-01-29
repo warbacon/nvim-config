71647904b162151d5ebc81736cfd78f60991df78
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-01-29 14:25:00 +0100

    refactor: make mason to depend in mason-lspconfig

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,58 +1,55 @@
 return {
-    -- MASON-LSPCONFIG ========================================================
-    { "williamboman/mason-lspconfig.nvim", lazy = true },
-
-    -- MASON ==================================================================
-    {
-        "williamboman/mason.nvim",
-        build = ":MasonUpdate",
-        event = "VeryLazy",
-        keys = {
-            { "<leader>m", "<cmd>Mason<cr>" },
-        },
-        opts = {
-            ui = {
-                icons = {
-                    package_pending = " ",
-                    package_installed = " ",
-                    package_uninstalled = " ",
-                },
-                height = 0.8,
-            },
-        },
-        config = function(_, opts)
-            require("mason").setup(opts)
-            require("mason-lspconfig").setup()
-
-            local ensure_installed = vim.list_extend({
-                "clang-format",
-                "prettierd",
-                "ruff",
-                "shellcheck",
-                "shfmt",
-                "stylua",
-            }, vim.tbl_keys(require("config.lsp-servers")))
-
-            local registry = require("mason-registry")
-
-            registry:on("package:install:success", function()
-                vim.defer_fn(function()
-                    vim.api.nvim_exec_autocmds("FileType", {
-                        buffer = vim.api.nvim_get_current_buf(),
-                    })
-                end, 100)
-            end)
-
-            registry.refresh(function()
-                local mappings = require("mason-lspconfig").get_mappings().lspconfig_to_mason
-                for _, package_name in ipairs(ensure_installed) do
-                    package_name = mappings[package_name] or package_name
-                    if not registry.is_installed(package_name) then
-                        local package = registry.get_package(package_name)
-                        package:install()
-                    end
-                end
-            end)
-        end,
+    "williamboman/mason.nvim",
+    dependencies = {
+        "williamboman/mason-lspconfig.nvim",
     },
+    build = ":MasonUpdate",
+    event = "VeryLazy",
+    keys = {
+        { "<leader>m", "<cmd>Mason<cr>" },
+    },
+    opts = {
+        ui = {
+            icons = {
+                package_pending = " ",
+                package_installed = " ",
+                package_uninstalled = " ",
+            },
+            height = 0.8,
+        },
+    },
+    config = function(_, opts)
+        require("mason").setup(opts)
+        require("mason-lspconfig").setup()
+
+        local ensure_installed = vim.list_extend({
+            "clang-format",
+            "prettierd",
+            "ruff",
+            "shellcheck",
+            "shfmt",
+            "stylua",
+        }, vim.tbl_keys(require("config.lsp-servers")))
+
+        local registry = require("mason-registry")
+
+        registry:on("package:install:success", function()
+            vim.defer_fn(function()
+                vim.api.nvim_exec_autocmds("FileType", {
+                    buffer = vim.api.nvim_get_current_buf(),
+                })
+            end, 100)
+        end)
+
+        registry.refresh(function()
+            local mappings = require("mason-lspconfig").get_mappings().lspconfig_to_mason
+            for _, package_name in ipairs(ensure_installed) do
+                package_name = mappings[package_name] or package_name
+                if not registry.is_installed(package_name) then
+                    local package = registry.get_package(package_name)
+                    package:install()
+                end
+            end
+        end)
+    end,
 }

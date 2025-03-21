4a9d99fb4790302dace9c41f38cf50ac98589c08
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-03-21 16:18:15 +0100

    LSP: add cssls and html

--- lua/config/servers.lua
+++ lua/config/servers.lua
@@ -3,15 +3,22 @@ return {
     basedpyright = {},
     bashls = {},
     clangd = {},
+    cssls = {
+        on_attach = function(client, bufnr)
+            local filename = vim.api.nvim_buf_get_name(bufnr)
+            if string.match(filename, "/waybar/.*%.css$") then
+                client.stop()
+            end
+        end,
+    },
     docker_compose_language_service = {},
     dockerls = {},
     emmet_language_server = {
         filetypes = { "css", "html", "php" },
     },
+    html = {},
     intelephense = {
-        init_options = {
-            globalStoragePath = vim.fn.stdpath("data"),
-        },
+        init_options = { globalStoragePath = vim.fn.stdpath("data") },
     },
     jsonls = {
         on_new_config = function(new_config)
@@ -21,20 +28,22 @@ return {
     lua_ls = {
         settings = {
             Lua = {
-                workspace = { checkThirdParty = false },
+                workspace = { checkThirdParty = false }
             },
         },
     },
     powershell_es = { autostart = false },
     tailwindcss = {
-        filetypes = { "css", "html", "astro" },
+        filetypes = { "css", "html", "astro" }
     },
+    vtsls = {},
     yamlls = {
         on_new_config = function(new_config)
             new_config.settings.yaml.schemas = require("schemastore").yaml.schemas()
         end,
         filetypes = { "yaml" },
-        settings = { yaml = { schemaStore = { enable = false, url = "" } } },
+        settings = {
+            yaml = { schemaStore = { enable = false, url = "" } }
+        },
     },
-    vtsls = {},
 }

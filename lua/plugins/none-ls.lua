efd33a1f2bbfe21b799a0d600524601ff0a07c28
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-03-22 16:15:29 +0100

    fix: make none_ls depend on mason

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,5 +1,8 @@
 return {
     "nvimtools/none-ls.nvim",
+    dependencies = {
+        "mason.nvim",
+    },
     ft = { "fish", "markdown" },
     opts = function()
         local null_ls = require("null-ls")

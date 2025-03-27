63b358821fe96bcf8d58f6118fe49e678658601d
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-03-27 15:58:35 +0100

    fix: handle blink.cmp disable

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -8,8 +8,10 @@ return {
     config = function()
         local servers = vim.deepcopy(require("config.servers"), true)
 
+        local has_blink, blink_cmp = pcall(require, "blink-cmp")
+
         for server_name, opts in pairs(servers) do
-            opts.capabilities = require("blink-cmp").get_lsp_capabilities()
+            opts.capabilities = has_blink and blink_cmp.get_lsp_capabilities(opts.capabilities or {}, true) or opts.capabilities
             require("lspconfig")[server_name].setup(opts)
         end
     end,

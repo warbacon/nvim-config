a275996a8cf9a4f642b7a1b4da7cc8aa05b07477
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-03-27 21:12:25 +0100

    perf: lazier highlight-colors

--- lua/plugins/highlight-colors.lua
+++ lua/plugins/highlight-colors.lua
@@ -1,6 +1,6 @@
 return {
     "brenoprata10/nvim-highlight-colors",
-    event = "BufReadPre",
+    event = "LazyFile",
     opts = {
         enable_short_hex = false,
         enable_hsl_without_function = false,
@@ -8,4 +8,10 @@ return {
         enable_tailwind = true,
         exclude_filetypes = { "lazy" },
     },
+    config = function(_, opts)
+        vim.defer_fn(function()
+            require("nvim-highlight-colors").setup(opts)
+            require("nvim-highlight-colors").turnOn()
+        end, 100)
+    end,
 }

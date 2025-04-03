acdf2d59a5556d7291a631710f22c1412a951932
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-04-03 23:39:54 +0200

    fix: better lsp capabilities

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -11,7 +11,8 @@ return {
         local has_blink, blink_cmp = pcall(require, "blink-cmp")
 
         for server_name, opts in pairs(servers) do
-            opts.capabilities = has_blink and blink_cmp.get_lsp_capabilities(opts.capabilities or {}, true) or opts.capabilities
+            opts.capabilities = has_blink and blink_cmp.get_lsp_capabilities(opts.capabilities or {}, true)
+                or opts.capabilities
             require("lspconfig")[server_name].setup(opts)
         end
     end,

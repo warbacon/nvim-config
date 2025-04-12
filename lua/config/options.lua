6bac1e4d8845d9aa16e3eb1937b1a1e3c45da90f
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-04-12 17:18:25 +0200

    feat: full folding support

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -56,3 +56,18 @@ vim.o.scrolloff = 5
 
 -- Limit the height of the popup menu
 vim.o.pumheight = 10
+
+-- Folding
+vim.o.foldlevel = 99
+vim.o.foldtext = ""
+vim.o.foldmethod = "expr"
+vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client and client:supports_method("textDocument/foldingRange") then
+            local win = vim.api.nvim_get_current_win()
+            vim.wo[win][0].foldexpr = "v:lua.vim.lsp.foldexpr()"
+        end
+    end,
+})

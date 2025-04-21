f00fad8ce6117aa9f24aaa1add60e5b7ace236f2
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-04-21 19:34:54 +0200

    +plug(fzf-lua): works poorly on windows though

---
+++ lua/plugins/fzf.lua
@@ -0,0 +1,39 @@
+return {
+    "ibhagwan/fzf-lua",
+    event = "VeryLazy",
+    cmd = "FzfLua",
+    keys = {
+        { "<leader>,", "<cmd>FzfLua buffers<cr>" },
+        { "<leader>f", "<cmd>FzfLua files<cr>" },
+        { "<leader>sg", "<cmd>FzfLua live_grep<cr>" },
+        { "<leader>sh", "<cmd>FzfLua helptags<cr>" },
+        { "<leader>sd", "<cmd>FzfLua diagnostics_workspace<cr>" },
+        { "<c-p>", "<cmd>FzfLua<cr>" },
+    },
+    opts = {
+        fzf_colors = true,
+        defaults = {
+            file_icons = "mini",
+            winopts = {
+                preview = {
+                    flip_columns = 120,
+                },
+            },
+        },
+        files = {
+            git_icons = true,
+            cwd_prompt = false,
+        },
+        keymap = {
+            builtin = {
+                ["<F1>"] = "toggle-help",
+                ["<M-m>"] = "toggle-fullscreen",
+                ["<M-p>"] = "toggle-preview",
+            },
+        },
+    },
+    config = function(_, opts)
+        require("fzf-lua").setup(opts)
+        require("fzf-lua").register_ui_select()
+    end,
+}

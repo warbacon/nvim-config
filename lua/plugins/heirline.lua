1a92cd6f8a776619f2cb12af85edc4c08638dd2e
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-04-24 22:19:14 +0200

    heirline: enable it back

--- lua/plugins/heirline.lua
+++ lua/plugins/heirline.lua
@@ -1,6 +1,5 @@
 return {
     "rebelot/heirline.nvim",
-    enabled = false,
     config = function()
         local conditions = require("heirline.conditions")
         local utils = require("heirline.utils")

326b4da0832996f5036c7554833194fcbe78cb14
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-04-25 14:51:53 +0200

    LSP(nixd): properly configure options

--- lsp/nixd.lua
+++ lsp/nixd.lua
@@ -1,2 +1,19 @@
 ---@type vim.lsp.Config
-return {}
+return {
+    settings = {
+        nixd = {
+            nixpkgs = {
+                expr = "import <nixpkgs> { }",
+            },
+            options = {
+                nixos = {
+                    expr = string.format(
+                        'import (builtins.getFlake "%s/Git/nixos-config").nixosConfigurations.%s.options',
+                        vim.env.HOME,
+                        vim.fn.hostname()
+                    ),
+                },
+            },
+        },
+    },
+}

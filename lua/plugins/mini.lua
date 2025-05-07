fd9ee0d9c56631bb0670e5aacc6a8e6d6b247054
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-05-07 20:32:18 +0200

    mini.files: add change cwd keybind

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -46,7 +46,28 @@ return {
                 end,
             },
         },
-        opts = function()
+        opts = {
+            content = {
+                filter = function(fs_entry)
+                    return not (fs_entry.fs_type == "directory" and fs_entry.name == ".git")
+                end,
+            },
+            windows = {
+                preview = true,
+                width_focus = 30,
+                width_preview = 40,
+            },
+            mappings = {
+                go_in = "L",
+                go_in_plus = "l",
+                go_out = "H",
+                go_out_plus = "h",
+                change_cwd = ",",
+            },
+        },
+        config = function(_, opts)
+            require("mini.files").setup(opts)
+
             vim.api.nvim_create_autocmd("User", {
                 pattern = "MiniFilesActionRename",
                 callback = function(event)
@@ -54,24 +75,21 @@ return {
                 end,
             })
 
-            return {
-                content = {
-                    filter = function(fs_entry)
-                        return not (fs_entry.fs_type == "directory" and fs_entry.name == ".git")
-                    end,
-                },
-                windows = {
-                    preview = true,
-                    width_focus = 30,
-                    width_preview = 40,
-                },
-                mappings = {
-                    go_in = "L",
-                    go_in_plus = "l",
-                    go_out = "H",
-                    go_out_plus = "h",
-                },
-            }
+            vim.api.nvim_create_autocmd("User", {
+                pattern = "MiniFilesBufferCreate",
+                callback = function(args)
+                    vim.keymap.set("n", opts.mappings and opts.mappings.change_cwd, function()
+                        local cur_entry_path = MiniFiles.get_fs_entry().path
+                        local cur_directory = vim.fs.dirname(cur_entry_path)
+                        if cur_directory ~= nil then
+                            vim.fn.chdir(cur_directory)
+                            vim.notify("cwd: " .. vim.fn.fnamemodify(cur_directory, ":~"), "info", {
+                                title = "MiniFiles",
+                            })
+                        end
+                    end, { buffer = args.data.buf_id })
+                end,
+            })
         end,
     },
 }

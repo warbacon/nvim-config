8882ca37c09bc908f325ce365ea82da9c378052a
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-05-19 17:15:04 +0200

    lspconfig: remove unnecesary start commands

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,7 +1,7 @@
 return {
     "neovim/nvim-lspconfig",
     event = "LazyFile",
-    cmd = { "LspInfo", "LspLog", "LspStop", "LspStart" },
+    cmd = { "LspInfo", "LspLog" },
     config = function()
         for server, opts in pairs(Util.lsp_servers) do
             vim.lsp.config(server, opts)

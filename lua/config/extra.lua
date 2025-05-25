82d0d3c9a1b7a70aac8b82e27fe4928265ce1f45
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-05-25 20:09:47 +0200

    perf: improve performance in WSL

--- lua/config/extra.lua
+++ lua/config/extra.lua
@@ -50,3 +50,21 @@ vim.filetype.add({
 vim.api.nvim_create_user_command("W", "w<bang>", { bang = true })
 vim.api.nvim_create_user_command("Q", "q<bang>", { bang = true })
 vim.api.nvim_create_user_command("Wq", "wq<bang>", { bang = true })
+
+-- HACK: move Windows entries to end of PATH in WSL to improve performance
+if vim.fn.has("wsl") == 1 then
+    local path_sep = ":"
+    local original_path = vim.env.PATH
+    local linux_paths = {}
+    local windows_paths = {}
+
+    for entry in string.gmatch(original_path, "([^" .. path_sep .. "]+)") do
+        if entry:match("^/mnt/") then
+            table.insert(windows_paths, entry)
+        else
+            table.insert(linux_paths, entry)
+        end
+    end
+
+    vim.env.PATH = table.concat(vim.list_extend(linux_paths, windows_paths), path_sep)
+end

89ffcfd3a516dcc9047c5245e4102f18220058a3
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-06-03 22:01:15 +0200

    fix: add version detection

--- init.lua
+++ init.lua
@@ -1,3 +1,27 @@
+--
+-- ███    ███ ██  ██████  ██    ██ ██ ███    ███
+-- ████  ████ ██ ██    ██ ██    ██ ██ ████  ████
+-- ██ ████ ██ ██ ██    ██ ██    ██ ██ ██ ████ ██
+-- ██  ██  ██ ██ ██    ██  ██  ██  ██ ██  ██  ██
+-- ██      ██ ██  ██████    ████   ██ ██      ██
+--
+-- Made by Warbacon
+-- https://github.com/warbacon/nvim-config
+
+if vim.fn.has("nvim-0.11") == 0 then
+    vim.defer_fn(function()
+        local v = vim.version()
+        local msg = string.format(
+            "This config requires Neovim v0.11.x or higher. You have v%d.%d.%d.",
+            v.major,
+            v.minor,
+            v.patch
+        )
+        vim.notify(msg, vim.log.levels.WARN)
+    end, 100)
+    return
+end
+
 _G.Util = require("util")
 
 require("config.options")

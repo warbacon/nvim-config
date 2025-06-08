6355d5d7f4257e41393583d8e7f3d8b179d80876
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-06-08 19:37:07 +0200

    refactor: add more descriptive types to icons table

--- lua/util.lua
+++ lua/util.lua
@@ -3,6 +3,12 @@ local M = {}
 M.is_win = vim.uv.os_uname().sysname:find("Windows") ~= nil
 M.is_lin = vim.uv.os_uname().sysname:find("Linux") ~= nil
 
+---@class Icons
+---@field indent string
+---@field signs string[]
+---@field kinds table<string, string>
+
+---@type Icons
 M.icons = {
     indent = "▏",
     signs = { "", "", "", "" },

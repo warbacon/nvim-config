915465206d759112f9d3039fe36d2c9c048c6dfc
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-06-20 00:15:24 +0200

    refactor: declare lsp servers in vim.g.lsp_servers

---
+++ lua/config/lsp.lua
@@ -0,0 +1,61 @@
+---@type table<string, vim.lsp.Config>
+vim.g.lsp_servers = {
+    astro = {},
+    basedpyright = {},
+    bashls = {
+        on_attach = function(client, bufnr)
+            local filename = vim.api.nvim_buf_get_name(bufnr)
+            local basename = vim.fn.fnamemodify(filename, ":t")
+            if basename:match("^%.env") then
+                ---@diagnostic disable-next-line: missing-parameter
+                client.stop()
+            end
+        end,
+    },
+    clangd = {},
+    cssls = {
+        on_attach = function(client, bufnr)
+            local filename = vim.api.nvim_buf_get_name(bufnr)
+            if string.match(filename, "/waybar/.*%.css$") then
+                ---@diagnostic disable-next-line: missing-parameter
+                client.stop()
+            end
+        end,
+    },
+    docker_compose_language_service = {},
+    dockerls = {},
+    emmet_language_server = {},
+    html = {},
+    jsonls = {
+        before_init = function(_, config)
+            config.settings = {
+                json = {
+                    schemas = require("schemastore").json.schemas(),
+                },
+            }
+        end,
+    },
+    lua_ls = {
+        settings = {
+            Lua = {
+                workspace = { checkThirdParty = false },
+            },
+        },
+    },
+    nixd = {},
+    svelte = {},
+    tailwindcss = {},
+    vtsls = {},
+    yamls = {
+        filetypes = { "yaml" },
+        settings = {
+            yaml = {
+                schemaStore = { enable = false, url = "" },
+            },
+        },
+        before_init = function(_, config)
+            ---@diagnostic disable-next-line: inject-field
+            config.settings.yaml.schemas = require("schemastore").yaml.schemas()
+        end,
+    },
+}
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,7 +1,7 @@
 return {
     "neovim/nvim-lspconfig",
     config = function()
-        for server, opts in pairs(Util.lsp_servers) do
+        for server, opts in pairs(vim.g.lsp_servers) do
             vim.lsp.config(server, opts)
             vim.lsp.enable(server)
         end

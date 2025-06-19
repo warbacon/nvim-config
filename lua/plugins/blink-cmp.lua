efbe4ea89ac99e3be8c9515a495628cee44b05f3
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-06-20 00:16:28 +0200

    blink.cmp: experiments :P

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -22,10 +22,46 @@ return {
                     module = "lazydev.integrations.blink",
                     score_offset = 100,
                 },
+                lsp = {
+                    fallbacks = { "path", "buffer" },
+                },
                 path = {
                     opts = {
                         show_hidden_files_by_default = true,
                     },
+                },
+                buffer = {
+                    -- keep case of first char
+                    transform_items = function(a, items)
+                        local keyword = a.get_keyword()
+                        local correct, case
+                        if keyword:match("^%l") then
+                            correct = "^%u%l+$"
+                            case = string.lower
+                        elseif keyword:match("^%u") then
+                            correct = "^%l+$"
+                            case = string.upper
+                        else
+                            return items
+                        end
+
+                        -- avoid duplicates from the corrections
+                        local seen = {}
+                        local out = {}
+                        for _, item in ipairs(items) do
+                            local raw = item.insertText
+                            if raw and raw:match(correct) then
+                                local text = case(raw:sub(1, 1)) .. raw:sub(2)
+                                item.insertText = text
+                                item.label = text
+                            end
+                            if not seen[item.insertText] then
+                                seen[item.insertText] = true
+                                table.insert(out, item)
+                            end
+                        end
+                        return out
+                    end,
                 },
             },
         },

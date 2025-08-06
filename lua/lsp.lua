d9bfe0d1cf04e2fb189bdbf2689bd4cccdabc05b
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-08-06 16:53:35 +0200

    keymaps: remove lsp custom keymaps

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -19,9 +19,6 @@ vim.api.nvim_create_autocmd("LspAttach", {
     callback = function(args)
         local client = vim.lsp.get_client_by_id(args.data.client_id)
 
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = args.buf })
-        vim.keymap.set("n", "gD", vim.lsp.buf.type_definition, { buffer = args.buf })
-
         if client and client:supports_method("textDocument/documentColor") then
             vim.lsp.document_color.enable(true, args.buf)
         end

4de757702769cabe51ebe430b43e9774ab3046da
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-08-07 13:35:48 +0200

    keymaps: esc to exit terminal mode

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,6 +5,9 @@ vim.keymap.set({ "n", "v" }, "<leader>Y", '"+y$')
 -- Update plugins
 vim.keymap.set("n", "<Leader>u", vim.pack.update)
 
+-- Use <Esc> to exit terminal mode
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
+
 -- Disable the `q:` command-line window
 vim.keymap.set("n", "q:", "<cmd>echoe 'q:'<CR>", { noremap = true })
 

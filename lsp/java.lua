11ac0cc5b8d9789111a6facd5b47445ee53a33bd
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-09-10 22:39:48 +0200

    java: support MPJ-EXPRESS

---
+++ lsp/java.lua
@@ -0,0 +1,12 @@
+---@type vim.lsp.Config
+return {
+    settings = {
+        java = {
+            project = {
+                referencedLibraries = {
+                    vim.env.MPJ_HOME and vim.env.MPJ_HOME .. "/lib/*.jar" or "",
+                },
+            },
+        },
+    },
+}

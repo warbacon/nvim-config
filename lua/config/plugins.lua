84edd9791fe03faa84e8cb6a57cba6d9da7b0f9e
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-10-19 20:19:32 +0200

    lazy: bootstrap lazy in plugins file

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -1,4 +1,24 @@
-Util.bootstrap_lazy()
+-- MADNESS:
+
+-- BOOTSTRAPING ------------------------------------------------------------{{{
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.uv.fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- }}}
 
 ---@type LazySpec
 local plugins = {
@@ -24,10 +44,15 @@ local plugins = {
     },
 }
 
+-- SETUP ------------------------------------------------------------------{{{
+
 require("lazy").setup(plugins, {
     install = { colorscheme = { "tokyonight-night" } },
-    ui = { border = vim.o.winborder },
+    ui = { border = "rounded" },
 })
+vim.keymap.set("n", "<Leader>l", require("lazy").show)
+
+-- }}}
 
 local later = require("mini.deps").later
 local now = require("mini.deps").now

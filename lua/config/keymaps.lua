8a7bfd5d96fe23d2fdd792b6f2c6098613562f78
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-10-21 19:52:36 +0200

    fix: disable restart keymap in nvim stable

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,7 +2,7 @@
 vim.api.nvim_create_autocmd("LspAttach", {
     callback = function()
         vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = true })
-    end
+    end,
 })
 
 -- Copy to system clipboard
@@ -17,7 +17,9 @@ vim.keymap.set({ "n", "v" }, "<leader>P", '"+P')
 vim.keymap.set("n", "<Leader>f", ":find **/", { noremap = true })
 
 -- Restart Neovim
-vim.keymap.set("n", "<Leader>rr", "<Cmd>restart<CR>")
+if vim.fn.has("nvim-0.12") then
+    vim.keymap.set("n", "<Leader>rr", "<Cmd>restart<CR>")
+end
 
 -- Use <Esc> to exit terminal mode
 vim.keymap.set("t", "<M-q>", "<C-\\><C-n>")

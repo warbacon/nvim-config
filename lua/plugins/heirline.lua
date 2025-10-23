ee7f3e81c7fc429933f7e68f3f90060b7fb8ea3e
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-10-23 09:26:22 +0200

    ui(heirline): add diagnostics

--- lua/plugins/heirline.lua
+++ lua/plugins/heirline.lua
@@ -130,6 +130,47 @@ return {
             { provider = "%<" },
         }
 
+        local Diagnostics = {
+            condition = conditions.has_diagnostics,
+            static = {
+                error_icon = vim.diagnostic.config()["signs"]["text"][vim.diagnostic.severity.ERROR],
+                warn_icon = vim.diagnostic.config()["signs"]["text"][vim.diagnostic.severity.WARN],
+                info_icon = vim.diagnostic.config()["signs"]["text"][vim.diagnostic.severity.INFO],
+                hint_icon = vim.diagnostic.config()["signs"]["text"][vim.diagnostic.severity.HINT],
+            },
+            init = function(self)
+                self.errors = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.ERROR })
+                self.warnings = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.WARN })
+                self.hints = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.HINT })
+                self.info = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.INFO })
+            end,
+            update = { "DiagnosticChanged", "BufEnter" },
+            {
+                provider = function(self)
+                    return self.errors > 0 and (self.error_icon .. " " .. self.errors .. " ")
+                end,
+                hl = "DiagnosticError",
+            },
+            {
+                provider = function(self)
+                    return self.warnings > 0 and (self.warn_icon .. " " .. self.warnings .. " ")
+                end,
+                hl = "DiagnosticWarn",
+            },
+            {
+                provider = function(self)
+                    return self.info > 0 and (self.info_icon .. " " .. self.info .. " ")
+                end,
+                hl = "DiagnosticInfo",
+            },
+            {
+                provider = function(self)
+                    return self.hints > 0 and (self.hint_icon .. " " .. self.hints .. " ")
+                end,
+                hl = "DiagnosticHint",
+            },
+        }
+
         local Ruler = {
             provider = "%-8.(%l:%c%V%) %P",
         }
@@ -140,6 +181,8 @@ return {
             Space,
             FileBlock,
             Align,
+            Diagnostics,
+            Space,
             Ruler,
             Space,
             ViMode,

8a02cf3e18c0e3c550e9e71561d5641cd627db6a
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-10-24 00:30:08 +0200

    tokyonight: use opts

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,18 +1,18 @@
 return {
     "folke/tokyonight.nvim",
     priority = 1000,
-    config = function()
-        ---@diagnostic disable-next-line: missing-fields
-        require("tokyonight").setup({
-            on_highlights = function(hl, c)
-                hl.PmenuBorder = { link = "FloatBorder" }
-                hl.MatchParen = {
-                    bg = c.fg_gutter,
-                    bold = true,
-                }
-            end,
-        })
-
+    ---@type tokyonight.Config
+    opts = {
+        on_highlights = function(hl, c)
+            hl.PmenuBorder = { link = "FloatBorder" }
+            hl.MatchParen = {
+                bg = c.fg_gutter,
+                bold = true,
+            }
+        end,
+    },
+    config = function(_, opts)
+        require("tokyonight").setup(opts)
         vim.cmd.colorscheme("tokyonight-night")
     end,
 }

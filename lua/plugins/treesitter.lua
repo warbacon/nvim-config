2732d7cbff1ef553fb79cc3cf8cf36feb951af5b
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2025-11-09 21:41:40 +0100

    treesitter: add textobjets and autoclose

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,74 +1,111 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    branch = "main",
-    lazy = vim.fn.argc(-1) == 0,
-    event = "VeryLazy",
-    build = function()
-        require("nvim-treesitter").update()
-    end,
-    config = function()
-        local ts_parsers = {
-            "astro",
-            "bash",
-            "c",
-            "cpp",
-            "css",
-            "fish",
-            "gitcommit",
-            "go",
-            "html",
-            "hyprlang",
-            "ini",
-            "java",
-            "javascript",
-            "json",
-            "jsonc",
-            "kitty",
-            "lua",
-            "markdown",
-            "markdown_inline",
-            "nix",
-            "printf",
-            "python",
-            "regex",
-            "svelte",
-            "toml",
-            "typescript",
-            "vim",
-            "vimdoc",
-            "xml",
-            "yaml",
-        }
+    {
+        "nvim-treesitter/nvim-treesitter",
+        branch = "main",
+        lazy = vim.fn.argc(-1) == 0,
+        event = "VeryLazy",
+        build = function()
+            require("nvim-treesitter").update()
+        end,
+        config = function()
+            local ts_parsers = {
+                "astro",
+                "bash",
+                "c",
+                "cpp",
+                "css",
+                "fish",
+                "gitcommit",
+                "go",
+                "html",
+                "hyprlang",
+                "ini",
+                "java",
+                "javascript",
+                "json",
+                "jsonc",
+                "kitty",
+                "lua",
+                "markdown",
+                "markdown_inline",
+                "nix",
+                "printf",
+                "python",
+                "regex",
+                "svelte",
+                "toml",
+                "typescript",
+                "vim",
+                "vimdoc",
+                "xml",
+                "yaml",
+            }
 
-        local installed_parsers = require("nvim-treesitter").get_installed()
+            local installed_parsers = require("nvim-treesitter").get_installed()
 
-        if vim.fn.has("win32") == 0 then
-            local to_install = vim.tbl_filter(function(lang)
-                return not installed_parsers[lang]
-            end, ts_parsers)
+            if vim.fn.has("win32") == 0 then
+                local to_install = vim.tbl_filter(function(lang)
+                    return not installed_parsers[lang]
+                end, ts_parsers)
 
-            if #to_install > 0 then
-                require("nvim-treesitter.install").install(to_install)
-            end
-        end
-
-        local filetypes = {}
-        for _, lang in ipairs(installed_parsers) do
-            for _, ft in ipairs(vim.treesitter.language.get_filetypes(lang)) do
-                table.insert(filetypes, ft)
-            end
-        end
-
-        vim.api.nvim_create_autocmd("FileType", {
-            pattern = filetypes,
-            callback = function(ev)
-                local lang = vim.treesitter.language.get_lang(ev.match)
-
-                vim.treesitter.start()
-                if lang and vim.treesitter.query.get(lang, "indents") then
-                    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                if #to_install > 0 then
+                    require("nvim-treesitter.install").install(to_install)
                 end
-            end,
-        })
-    end,
+            end
+
+            local filetypes = {}
+            for _, lang in ipairs(installed_parsers) do
+                for _, ft in ipairs(vim.treesitter.language.get_filetypes(lang)) do
+                    table.insert(filetypes, ft)
+                end
+            end
+
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = filetypes,
+                callback = function(ev)
+                    local lang = vim.treesitter.language.get_lang(ev.match)
+
+                    vim.treesitter.start()
+                    if lang and vim.treesitter.query.get(lang, "indents") then
+                        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                    end
+                end,
+            })
+        end,
+    },
+    {
+        "windwp/nvim-ts-autotag",
+        opts = {},
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        branch = "main",
+        opts = {
+            select = {
+                lookahead = true,
+                selection_modes = {
+                    ["@parameter.outer"] = "v",
+                    ["@function.outer"] = "V",
+                    ["@class.outer"] = "<c-v>",
+                },
+                include_surrounding_whitespace = false,
+            },
+        },
+        config = function(_, opts)
+            require("nvim-treesitter-textobjects").setup(opts)
+
+            vim.keymap.set({ "x", "o" }, "af", function()
+                require("nvim-treesitter-textobjects.select").select_textobject("@function.outer", "textobjects")
+            end)
+            vim.keymap.set({ "x", "o" }, "if", function()
+                require("nvim-treesitter-textobjects.select").select_textobject("@function.inner", "textobjects")
+            end)
+            vim.keymap.set({ "x", "o" }, "ac", function()
+                require("nvim-treesitter-textobjects.select").select_textobject("@class.outer", "textobjects")
+            end)
+            vim.keymap.set({ "x", "o" }, "ic", function()
+                require("nvim-treesitter-textobjects.select").select_textobject("@class.inner", "textobjects")
+            end)
+        end,
+    },
 }

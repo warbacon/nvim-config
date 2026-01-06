64a8167cfb4eaf6750ecbbe6ef053f08fd658b16
Joaquín Guerra <joaquinguerratocino@gmail.com>
2026-01-07 00:11:46 +0100

    feat(conform): more formatters

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -135,6 +135,9 @@ require("conform").setup({
     },
     formatters_by_ft = {
         lua = { "stylua" },
+        toml = { "taplo" },
+        ["*"] = { "injected" },
+        ["_"] = { "trim_whitespace", "trim_newlines", lsp_format = "prefer" },
     },
 })
 

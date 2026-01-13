7823128880939274a4124cc2a1ce168545d61b24
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2026-01-13 01:37:33 +0100

    feat(markdown): set conceallevel 3

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -3,5 +3,6 @@ vim.bo.textwidth = 80
 vim.api.nvim_create_autocmd("BufEnter", {
     callback = function()
         vim.wo.colorcolumn = vim.bo.filetype == "markdown" and "80" or ""
+        vim.wo.conceallevel = vim.bo.filetype == "markdown" and 3 or 0
     end,
 })

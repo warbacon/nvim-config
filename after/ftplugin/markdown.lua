f096f660be6f5c85808868614b0ed9dbf854e4ce
Joaquín Guerra <joaquinguerratocino@gmail.com>
2026-04-01 16:13:57 +0200

    feat(LSP): rumdl is annoying

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,11 +1,5 @@
 vim.bo.textwidth = 80
 
-vim.api.nvim_create_autocmd("LspAttach", {
-    callback = function()
-        vim.bo.formatexpr = ""
-    end,
-})
-
 vim.api.nvim_create_autocmd("BufEnter", {
     callback = function()
         vim.wo.colorcolumn = vim.bo.filetype == "markdown" and "80" or ""
